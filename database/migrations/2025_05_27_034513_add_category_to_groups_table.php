771dc02f0d4eb62c9a9860769e734b905e177ce4
rodrigocardenas <rodrigo.cardenasplaza@gmail.com>
2025-05-28 02:01:47 +0200

    Agregar columna 'category' a la tabla 'groups' con valor predeterminado 'official' y permitir su eliminación en la migración correspondiente.

--- database/migrations/2025_05_27_034513_add_category_to_groups_table.php
+++ database/migrations/2025_05_27_034513_add_category_to_groups_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('groups', function (Blueprint $table) {
-            //
+            $table->string('category')->default('official');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('groups', function (Blueprint $table) {
-            //
+            $table->dropColumn('category');
         });
     }
 };
