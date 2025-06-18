8fdf851937853501011d5356fc56b23fce5f28b6
rodrigocardenas <rodrigo.cardenasplaza@gmail.com>
2025-06-18 15:43:35 +0200

    Agregar registro de información del usuario en el método update del PushTokenController para mejorar la trazabilidad de las suscripciones a notificaciones push.

--- app/Http/Controllers/PushTokenController.php
+++ app/Http/Controllers/PushTokenController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class PushTokenController extends Controller
 {
@@ -14,6 +15,7 @@ class PushTokenController extends Controller
         ]);
 
         $user = Auth::user();
+        Log::info('user', ['user' => $user], $user->pushSubscriptions);
 
         // Guardar o actualizar el token en la relación pushSubscriptions
         $user->pushSubscriptions()->first()->updateOrCreate(
