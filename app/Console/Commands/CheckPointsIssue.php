faedab6ded1f2f1e2a0d731e15d1d0110ef58e85
rodrigocardenas <rodrigo.cardenasplaza@gmail.com>
2025-08-05 14:40:32 +0200

    Modificar la consulta en CheckPointsIssue para filtrar partidos por fecha de creación, asegurando que solo se consideren aquellos creados después del 1 de agosto de 2025 y que estén finalizados, mejorando la precisión en la verificación de resultados.

--- app/Console/Commands/CheckPointsIssue.php
+++ app/Console/Commands/CheckPointsIssue.php
@@ -35,7 +35,7 @@ class CheckPointsIssue extends Command
         $questionsWithAnswers = Question::whereHas('answers')
             ->whereNotNull('result_verified_at')
             ->whereHas('football_match', function($query) {
-                $query->whereIn('status', ['FINISHED', 'Match Finished']);
+                $query->where('created_at', '>', '2025-08-01')->whereIn('status', ['FINISHED', 'Match Finished']);
             })
             ->with(['answers.user', 'answers.questionOption', 'options', 'football_match'])
             ->get();
