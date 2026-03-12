b115680b2b44b74303422e1fd1bf8d04cc070fbf
rodrigocardenas <rodrigocardplaz_12@offsideclub.com>
2026-03-12 02:10:57 +0100

    style: Add carousel CSS styles to groups index view

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -356,4 +356,14 @@
         };
     </script>
 
+    <style>
+        .hide-scrollbar::-webkit-scrollbar {
+            display: none;
+        }
+        .hide-scrollbar {
+            -ms-overflow-style: none;
+            scrollbar-width: none;
+        }
+    </style>
+
 </x-dynamic-layout>
