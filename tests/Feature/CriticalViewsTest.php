6c12ed0aad309f49760adc7d4e468448f7825ce5
rodrigocardenas <rodrigocardplaz_12@offsideclub.com>
2026-03-30 23:55:22 +0200

    fix: use RefreshDatabase instead of DatabaseTransactions with MySQL

--- tests/Feature/CriticalViewsTest.php
+++ tests/Feature/CriticalViewsTest.php
@@ -8,14 +8,14 @@ use App\Models\Group;
 use App\Models\Competition;
 use App\Models\Question;
 use App\Models\Answer;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 /**
  * @group deploy
  */
 class CriticalViewsTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
 
     protected $user;
     protected $group;
